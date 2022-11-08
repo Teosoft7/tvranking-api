e0906928c14a1d43f807c2c61c548039314ff87e
Teosoft7 <thjeon.wa@gmail.com>
2022-11-07 16:10:23 -0800

    add settings for gcp deployment

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from web_scrap import TVRankScraper
+import os
 
 app = Flask(__name__)
 scrapper = TVRankScraper()
@@ -29,5 +30,5 @@ def ratings_route():
     )
 
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
