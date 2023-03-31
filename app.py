5b20791a1b47dbd6fd6668f1a33440f1148cf581
Teosoft7 <thjeon.wa@gmail.com>
2023-03-31 23:10:04 +0900

    clean the code

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, jsonify
-from web_scrap import TVRankScraper
 import os
+from flask import Flask, jsonify
 from flask_cors import CORS
+from web_scrap import TVRankScraper
+
 
 app = Flask(__name__)
 CORS(app)
@@ -15,14 +16,24 @@ def index():
         message='Welcome to the TV Ranking API'
     )
 
+
 @app.route('/ratings/<date>/<category>/<area>', methods=['GET'])
 def ratings_date_route(date, category, area):
     print(f'ratings with date : {date}, {category}, {area}')
-    
+
     ratings = scrapper.get_ratings(date, category, area)
 
     return jsonify(
         ratings
+    )
+
+
+@app.route('/last8ratings/<channel>/<programme>', methods=['GET'])
+def ratings_last8(channel, programme):
+    print(f'last 8 ratings of program : {channel}, {programme}')
+    return jsonify(
+        status=True,
+        message="Last 8 ratings for program"
     )
 
 
